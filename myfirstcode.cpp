a55b08b85bf0b30ef140b8e2b6673f6dc7434081
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2019-08-31 00:26:54 +0530

    Add files via upload

---
+++ myfirstcode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=2;
+	float b=3.5;
+	char c='A';
+	double d=3.56789;
+	cout<<"Hello World"<<"\n";
+	cin>>a;
+	cout<<a;
+	// cout<<d<<"\n"<<c<<"\n"<<b;
+
+
+	return 0; 
+}
