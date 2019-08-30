c61306699a9b33a70ad1eba137a6b9775cb3a136
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2019-08-31 00:47:05 +0530

    Update myfirstcode.cpp

--- myfirstcode.cpp
+++ myfirstcode.cpp
@@ -1,18 +1,64 @@
-#include<iostream>
+// #include<iostream>
 
+// using namespace std;
+
+// int main()
+// {
+// 	int a=2;
+// 	float b=3.5;
+// 	char c='A';
+// 	double d=3.56789;
+// 	cout<<"Hello World"<<"\n";
+// 	cin>>a;
+// 	cout<<a;
+// 	// cout<<d<<"\n"<<c<<"\n"<<b;
+
+
+// 	return 0; 
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+// 	int i=5;
+// 	i>10 ? cout<<"Greater": cout<<"Smaller";
+
+
+
+
+
+// 	return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	int i=5;
+// 	if(i>10){
+// 		cout<<"Greater";
+// 	}
+// 	else
+// 		cout<<"Smaller";
+// 	return 0;
+//}
+
+
+#include <iostream>
 using namespace std;
+int main(){
+	int i=21;
+	switch(i){
+		case 10: cout<<"Value is 10";
+		break;
+		case 15: cout<<"Value is 15";
+		break;
+		default: cout<<"Not Found";
+	}
 
-int main()
-{
-	int a=2;
-	float b=3.5;
-	char c='A';
-	double d=3.56789;
-	cout<<"Hello World"<<"\n";
-	cin>>a;
-	cout<<a;
-	// cout<<d<<"\n"<<c<<"\n"<<b;
-
-
-	return 0; 
+	return 0;
 }
+
+
+
