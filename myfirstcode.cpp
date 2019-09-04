b0ba03d817f217d9deffe816b596bd64f364e8df
Gautam Arora <51836173+Gautam2086@users.noreply.github.com>
2019-09-05 01:20:15 +0530

    Update myfirstcode.cpp

--- myfirstcode.cpp
+++ myfirstcode.cpp
@@ -24,11 +24,6 @@
 // int main(){
 // 	int i=5;
 // 	i>10 ? cout<<"Greater": cout<<"Smaller";
-
-
-
-
-
 // 	return 0;
 // }
 
@@ -45,20 +40,82 @@
 //}
 
 
+// #include <iostream>  
+// using namespace std;  
+// int main()  
+// {  
+// int n, reverse=0, rem;    
+// cout<<"Enter a number: ";    
+// cin>>n;    
+//   while(n!=0)    
+//   {    
+//      rem=n%10;      
+//      reverse=reverse*10+rem;    
+//      n/=10;    
+//   }    
+//  cout<<"Reversed Number: "<<reverse<<endl;     
+// return 0;  
+// }  
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+// 	int i=21;
+// 	switch(i){
+// 		case 10: cout<<"Value is 10";
+// 		break;
+// 		case 21: cout<<"Value is 21";
+// 		break;
+// 		default: cout<<"Error";
+// 	}
+
+
+// 	return 0;
+// }
+
+
+
+
+// #include<iostream> //CODING BLOCKS
+// using namespace std;
+
+// int main()
+// {
+// 	int n, arr[1000];
+// 	cout<<"Enter value of n:";
+// 	cin>>n;
+// 	return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
+
 int main(){
-	int i=21;
-	switch(i){
-		case 10: cout<<"Value is 10";
-		break;
-		case 15: cout<<"Value is 15";
-		break;
-		default: cout<<"Not Found";
+	int n;
+	cin>>n;
+	int a=5;
+	int d=2;
+	int an=0;
+	int sn=0;
+	an=a+(n-1)*d;
+	for(int i=1;i<=n;i++){
+
+		
 	}
 
 	return 0;
 }
+
+
+
+
+
+
+
 
 
 
